7365e774954a0f6de145c9b62b2600cf3197c45e
B144319 <s1897022@ed.ac.uk>
2024-10-12 16:05:58 +0100

    The checkout works

--- my_cool_script.sh
+++ my_cool_script.sh
@@ -7,4 +7,5 @@ echo -e "this is my\nReally \nCool \nScript!"
 echo I love bash
 echo I love nano too
 echo Version control is really neat.
-echo Does this work
+echo Does the checkout work? yes it does 
+
